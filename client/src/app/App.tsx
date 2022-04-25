3442d2ca5a2baa7b0313e7ea44efc1042928f673
Rodrigo-Tibirica <47817462+Rodrigo-Tibirica@users.noreply.github.com>
2022-04-25 15:07:31 -0300

    Tela Login

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -5,23 +5,25 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Home } from "../components/pages/Home";
 import { ProductScreen } from "../components/pages/ProductScreen";
+import { Signin }  from "../components/pages/signin/Login";
 function App() {
     return (
-        <>
+
+         <>   
             <Header />
-            <main className="py-3">
-                <Container>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route
-                            path="/product/:id"
-                            element={<ProductScreen />}
-                        />
-                    </Routes>
-                </Container>
-            </main>
-            <Footer />
-        </>
+             <main className="py-3">
+                 <Container>
+                     <Routes>
+                         <Route path="/" element={<Home />} />
+                         <Route
+                             path="/product/:id"
+                             element={<ProductScreen />}
+                         />
+                     </Routes>
+                 </Container>
+             </main>
+             <Footer />
+         </>
     );
 }
 
